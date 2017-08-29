03b2483ed7a0b881c464bebd89d0739005030d0e
Anh Quan Nguyen <anh.nguyen1@dawsoncollege.qc.ca>
2017-08-29 15:02:52 -0400

    Create and test Address

---
+++ Common/src/lib/Address.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package lib;
+
+/**
+ * @author 1632647
+ *
+ */
+public class Address {
+    private String civicNumber;
+    private String streetName;
+    	
+    public Address() { }
+
+    public Address (String civicNumber, String streetName)    {
+    	this.civicNumber = civicNumber;
+    	this.streetName = streetName;
+    }
+    
+    public String getAddress()  {
+    	return civicNumber + " " + streetName;
+    }
+
+    public String getCivicNumber()  {
+    	return civicNumber;
+    }
+    
+    public String getStreetName()    {
+    	return streetName;
+    }
+}
+
---
+++ Common/test/lib/AddressTest.java
@@ -0,0 +1,8 @@
+package lib;
+
+public class AddressTest {
+	public static void main(String[] args) {
+		Address myAddress = new Address("3040", "Sherbrooke");
+		System.out.println(myAddress.getAddress());
+	}
+}
